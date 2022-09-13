e8c14c3d02d9c0f0db102f26392c44bbf7adf449
Wily9 <william.mh.99@gmail.com>
2022-09-12 21:15:01 -0400

    Basic Math controller param sending

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -1,8 +1,24 @@
+const path = require('path');
+const fs = require('fs');
 module.exports =
     class MathsController extends require('./Controller') {
         constructor(HttpContext) {
             super(HttpContext);
         }
         get() {
+            if(this.HttpContext.path.queryString == '?'){
+                // Send helpPage
+                let helpPagePath = path.join(process.cwd(), "wwwroot/helpPages/mathsServiceHelp.html");
+                let content = fs.readFileSync(helpPagePath);
+                this.HttpContext.response.content("text/html", content);
+            }
+            else{
+                if(this.HttpContext.path.params.op){
+                        
+                } else {
+                    this.HttpContext.path.params.error = "Parameter 'op' is missing";
+                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                }
+            }
         }
     }
