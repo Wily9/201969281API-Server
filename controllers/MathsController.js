fbdec4df53a6f8f33613b6cd5c8fe26674b5a0f8
Wily9 <william.mh.99@gmail.com>
2022-09-12 20:46:02 -0400

    MathsController changer

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -1,24 +1,8 @@
-const MathModel = require('../models/math');
-const Repository = require('../models/repository');
 module.exports =
     class MathsController extends require('./Controller') {
         constructor(HttpContext) {
             super(HttpContext);
-            this.repository = new Repository(new MathModel());
         }
-        get(id) {
-            if (this.repository != null) {
-                if (!isNaN(id)) {
-                    let data = this.repository.get(id);
-                    if (data != null)
-                        this.HttpContext.response.JSON(data);
-                    else
-                        this.HttpContext.response.notFound();
-                }
-                else
-                    this.HttpContext.response.JSON(this.repository.getAll());
-            }
-            else
-                this.HttpContext.response.notImplemented();
+        get() {
         }
     }
